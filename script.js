b67b1fa96c56d92698ea303013ccce88d414774e
Ranu <88536226+Ranu2211@users.noreply.github.com>
2025-01-05 16:24:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded",()=>{
+	const target = document.getElementById('level');
+	let domlevel= 1;
+	let curr= target;
+	while(curr.parentElement){
+		domlevel++;
+		curr = curr.parentElement;
+	}
+	alert(`The level of the element is: ${domLevel}`);
+})
